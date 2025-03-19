a8bd393f4bc26786812e263cb61cac0a7736faf2
ehsan124511 <mstfwyahsan3@gmail.com>
2025-03-19 12:26:29 +0330

    data structure & algoritm exersize

---
+++ bazi_morakab.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+int main()
+{
+    int n, target;
+    std::cin >> n >> target;
+    int *arr = new int[n];
+
+    for (size_t i = 0; i < n; i++)
+    {
+        std::cin>>arr[i];
+    }
+    
+    
+    bool flag = false;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = i+1; j < n; j++)
+        {
+            if (arr[i]+arr[j] == target)
+            {
+                flag = true;
+                break;
+            }
+            
+        }
+        
+    }
+
+    if (flag == true)
+    {
+        std::cout << "true";
+    }
+    else 
+
+        std::cout << "false";
+
+        return 0;
+
+}
